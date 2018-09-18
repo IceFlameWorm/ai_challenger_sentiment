a2edb00dcb43615f4156e62844bf72a7eeff28bf
Aaron <empty@email.com>
2018-09-18 13:19:53 +0000

    set project env

---
+++ env.py
@@ -0,0 +1,13 @@
+from __future__ import print_function
+from __future__ import division
+
+import os
+
+PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
+DATASET_PATH = os.path.join(PROJECT_PATH, 'dataset')
+TRAINSET_PATH = os.path.join(DATASET_PATH, 'ai_challenger_sentiment_analysis_trainingset_20180816')
+VALSET_PATH = os.path.join(DATASET_PATH, 'ai_challenger_sentiment_analysis_validationset_20180816')
+TESTSET_PATH = os.path.join(DATASET_PATH, 'ai_challenger_sentiment_analysis_testa_20180816')
+TRAINSET_CSV = os.path.join(TRAINSET_PATH, 'sentiment_analysis_trainingset.csv')
+VALSET_CSV = os.path.join(VALSET_PATH, 'sentiment_analysis_validationset.csv')
+TESTSET_CSV = os.path.join(TESTSET_PATH, 'sentiment_analysis_testa.csv')

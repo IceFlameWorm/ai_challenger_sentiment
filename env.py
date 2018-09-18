ce2a5d97d7d9d4f4363b1ddb278c97c7a38a35c8
Aaron <empty@email.com>
2018-09-18 13:29:12 +0000

    update project env

--- env.py
+++ env.py
@@ -4,7 +4,8 @@ from __future__ import division
 import os
 
 PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
-DATASET_PATH = os.path.join(PROJECT_PATH, 'dataset')
+DATA_PATH = os.path.join(PROJECT_PATH, 'data')
+DATASET_PATH = os.path.join(DATA_PATH, 'dataset')
 TRAINSET_PATH = os.path.join(DATASET_PATH, 'ai_challenger_sentiment_analysis_trainingset_20180816')
 VALSET_PATH = os.path.join(DATASET_PATH, 'ai_challenger_sentiment_analysis_validationset_20180816')
 TESTSET_PATH = os.path.join(DATASET_PATH, 'ai_challenger_sentiment_analysis_testa_20180816')

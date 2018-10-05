93f0f78e4ed2fe71e026bde223f10a1724433fa2
Aaron <linan_sjtu_1987@163.com>
2018-10-05 16:24:14 +0800

    train wrapped linxi model with pretrained word vec

--- models/linxi.py
+++ models/linxi.py
@@ -3,15 +3,15 @@ import numpy as np
 from sklearn.utils import class_weight
 from keras.models import Model, Sequential, load_model
 from keras.layers import Input, Embedding, Dense, Conv1D, MaxPooling1D, GlobalMaxPooling1D
-from keras.layers import Dropout, BatchNormalization, SpatialDropout1D
+from keras.layers import Dropout, BatchNormalization, SpatialDropout1D, Concatenate
 from keras.optimizers import Adam
 from sklearn.metrics import f1_score, accuracy_score
 from keras.utils import to_categorical
 from keras.callbacks import ModelCheckpoint, EarlyStopping, TensorBoard, ReduceLROnPlateau, CSVLogger
 from env import *
 
-from utils.metrics import macro_f1
-from utils.clr_callback import CyclicLR
+#from utils.metrics import macro_f1
+from utils.callbacks import *
 
 from .base import SingleModel, CompositeModel
 
@@ -120,7 +120,7 @@ def main():
 class LinXiSingleModel(SingleModel):
     def _build(self, *args, **kwargs):
         maxlen = kwargs['maxlen']
-
+        embedding = kwargs.get('embedding')
         # model = Sequential()
         # embedding_dim = 128
         # model.add(Embedding(50000, embedding_dim,input_length=maxlen))
@@ -133,7 +133,10 @@ class LinXiSingleModel(SingleModel):
         # # model.add(layers.Dense(32, activation='relu'))
         # model.add(Dense(4, activation='softmax'))
         inp = Input(shape=(maxlen,))
-        out = Embedding(50000, 128)(inp)
+        if embedding is None:
+            out = Embedding(50000, 100)(inp)
+        else:
+            out = Embedding(50000, 100, weights = [embedding], trainable = False)(inp)
         #out = SpatialDropout1D(0.5)(out)
         out = Conv1D(64, 3, activation='relu')(out)
         out = MaxPooling1D(5)(out)
@@ -143,35 +146,65 @@ class LinXiSingleModel(SingleModel):
         #out = Dropout(0.5)(out)
         out = GlobalMaxPooling1D()(out)
         #out = BatchNormalization()(out)
+        
+#        out1 = Conv1D(32, 3, activation='relu')(out)
+#        out1 = GlobalMaxPooling1D()(out1)
+#        out2 = Conv1D(32, 5, activation='relu')(out)
+#        out2 = GlobalMaxPooling1D()(out2)
+#        out3 = Conv1D(32, 7, activation='relu')(out)
+#        out3 = GlobalMaxPooling1D()(out3)
+#        out = Concatenate()([out1, out2, out3])
+        
         out = Dense(4, activation='softmax')(out)
         model = Model(inputs = inp, outputs = out)
+        #model.summary()
         return model
 
 
     def fit(self, inputs, outputs, *args, **kwargs):
         lr = 1e-3
-        EPOCHS = 1
+        EPOCHS = 100
         BATCH_SIZE = 64
         PATIENCE = 6
+        
+        train_x = inputs
+        y_train_onehot = outputs
+        val_x, y_val_onehot = kwargs['validation_data']
+        
+        train_sample_weights = class_weight.compute_sample_weight('balanced', y_train_onehot)
+
         model_file = kwargs['model_file']
+        
         self._model.compile(optimizer= Adam(lr),loss='categorical_crossentropy',metrics=['acc'])
-        cbs = [ModelCheckpoint(model_file, monitor= 'val_acc', save_best_only=True, mode = 'max'),
-               # EarlyStopping(patience=PATIENCE),
-               # ReduceLROnPlateau('val_loss', factor=0.2, verbose=1, patience=int(PATIENCE / 2)),
+        cbs = [
+#                ModelCheckpoint(model_file, monitor= 'val_acc', save_best_only=True, mode = 'max'),
+                CustomModelCheckPoint(model_file,
+                                      monitor = self._f1_monitor,
+                                      save_best_only = True,
+                                      mode = 'max',
+                                      verbose = 1,
+                                      val_data = (val_x, y_val_onehot)),
+                EarlyStopping(patience=PATIENCE),
+                ReduceLROnPlateau('val_loss', factor=0.1, verbose=1, patience=int(PATIENCE / 2)),
                #CyclicLR(base_lr = lr, max_lr = 10 * lr, step_size = 1000,
                         #mode = 'exp_range', gamma=0.99994
                #        )
               ]
-        train_x = inputs
-        y_train_onehot = outputs
-        val_x, y_val_onehot = kwargs['validation_data']
         history = self._model.fit(train_x, y_train_onehot, epochs = EPOCHS,
                             batch_size = BATCH_SIZE, validation_data = (val_x, y_val_onehot,
                                                                         #val_sample_weights
                                                                        ),
                             callbacks = cbs,
-                            #sample_weight = train_sample_weights,
+                            sample_weight = train_sample_weights,
                             verbose = 1)
+                            
+    def _f1_monitor(self, y_true, y_pred):
+        y_pred = np.argmax(y_pred, axis = -1)
+        y_true = np.argmax(y_true, axis = -1)
+
+        f1 = f1_score(y_true, y_pred, average='macro')
+        return f1
+            
 
     def _load(self, model_file, weights_file = None):
         return load_model(model_file)
@@ -190,6 +223,9 @@ class LinXiCompositeModel(CompositeModel):
         seq = kwargs['seq']
 
         train_x = inputs
+        SAVED_PATH = os.path.join(SAVED_MODELS_PATH, 'linxi')
+        if not os.path.exists(SAVED_PATH):
+            os.makedirs(SAVED_PATH)
 
         for i, comp in enumerate(self._comps):
             col = y_cols[i]
@@ -197,10 +233,10 @@ class LinXiCompositeModel(CompositeModel):
             val_y = val_outputs[col] + 2
             y_train_onehot = to_categorical(train_y)
             y_val_onehot = to_categorical(val_y)
-            MODEL_PRE = os.path.join(SAVED_MODELS_PATH, 'linxi', 'cnn_')
+            MODEL_PRE = os.path.join(SAVED_PATH, 'cnn_')
             model_file = MODEL_PRE + str(i) + '.h5'
             comp.fit(train_x, y_train_onehot, model_file = model_file, validation_data = (val_x, y_val_onehot))
-            comp._load(model_file)
+            comp.load_weights(model_file)
 
             y_val_pred = comp.predict(val_x)
 
@@ -208,10 +244,10 @@ class LinXiCompositeModel(CompositeModel):
             print("The %sth grain: f1_score - %s, acc - %s" % (i, F1_score, accuracy_score(val_y, y_val_pred)))
 
     def predict(self, inputs, *args, **kwargs):
-        seq = kwargs['seq']
-        xs = np.array(inputs[seq].values.tolist(), dtype = np.int)
+#        seq = kwargs['seq']
+#        xs = np.array(inputs[seq].values.tolist(), dtype = np.int)
 
-        outs = super(LinXiCompositeModel, self).predict(xs)
+        outs = super(LinXiCompositeModel, self).predict(inputs)
         return outs
 
 

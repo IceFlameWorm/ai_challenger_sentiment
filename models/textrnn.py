db078b6867dd6ae75722134b93cd24ed296f36c1
Aaron <empty@email.com>
2018-10-14 01:05:28 +0000

    add BiCuDNNGRUSeq

--- models/textrnn.py
+++ models/textrnn.py
@@ -57,18 +57,35 @@ class GRULast(BaseRNN):
         out = Concatenate()([out1, out2])
         return out
 
-class BiCuDNNGRU(BaseRNN):
+class BiCuDNNGRULast(BaseRNN):
     def _rnn(self, inp1, inp2):
         rnn_dim = 100
         shared_bigru = Bidirectional(CuDNNGRU(rnn_dim, return_sequences=False))
-        shared_shrink = Lambda(lambda x: K.max(x, axis = 1),
-                               output_shape = (2 *rnn_dim, ))
+        #shared_shrink = Lambda(lambda x: K.max(x, axis = 1),
+        #                       output_shape = (2 *rnn_dim, ))
 
         out1 = shared_bigru(inp1)
         #out1 = shared_shrink(out1)
 
         out2 = shared_bigru(inp2)
         #out2 = shared_shrink(out2)
+
+        out = Concatenate()([out1, out2])
+        #out = Dropout(0.5)(out)
+        return out
+
+class BiCuDNNGRUSeq(BaseRNN):
+    def _rnn(self, inp1, inp2):
+        rnn_dim = 100
+        shared_bigru = Bidirectional(CuDNNGRU(rnn_dim, return_sequences=False))
+        shared_shrink = Lambda(lambda x: K.mean(x, axis = 1),
+                               output_shape = (2 *rnn_dim, ))
+
+        out1 = shared_bigru(inp1)
+        out1 = shared_shrink(out1)
+
+        out2 = shared_bigru(inp2)
+        out2 = shared_shrink(out2)
 
         out = Concatenate()([out1, out2])
         out = Dropout(0.5)(out)
